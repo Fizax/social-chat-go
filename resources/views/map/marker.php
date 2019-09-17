e5b9df62cfcd0b03d9eb6ee4762559b4a40ddea2
Bas <supersiku1234@hotmail.nl>
2019-09-17 14:02:54 +0200

    Query coords

--- resources/views/map/marker.php
+++ resources/views/map/marker.php
@@ -2,17 +2,43 @@
 
 header('Content-Type: application/json');
 
-/* data uit database terug naar vorig form */
+
+$dbHost = 'localhost';
+$dbName = 'socialapp';
+$dbUser = 'root';
+$dbPass = '';
+
+$db = new PDO(
+    "mysql:host=$dbHost;dbname=$dbName",
+    $dbUser,
+    $dbPass
+);
+// hier vang die de error op
+$db->setAttribute(
+    PDO::ATTR_ERRMODE,
+    PDO::ERRMODE_EXCEPTION
+);
+/* data uit database terug naar vorig form
+Haal alle data op uit de user tabellen welke beschikbaar zijn
 $marker = array(
     ['lat'=>1.7788481178788205,'lng'=>51.60656920854833],
     ['lat'=>3.7788481178788205,'lng'=>51.60656920854833],
     ['lat'=>2.7788481178788205,'lng'=>51.60656920854833]
 );
+*/
+
+$select = ("SELECT lat, lon FROM users");
+
+
+var_dump($select);
+die;
 
 
 
-/*data vanaf het formulier, dus update user table data */
+
+/*data vanaf het formulier, dus update user table data
+Plaats deze data in de user tabel doormiddel vam een update */
 $lat = $_GET['lat'];
 $lon = $_GET['lng'];
 
-echo json_encode($marker);
+//echo json_encode($marker);
--- resources/views/marker.blade.php
+++ resources/views/marker.blade.php
@@ -1,17 +1,41 @@
 <?php
+
 header('Content-Type: application/json');
+//
+//
+//$dbHost = 'localhost';
+//$dbName = 'socialapp';
+//$dbUser = 'root';
+//$dbPass = '';
+//
+//$db = new PDO(
+//    "mysql:host=$dbHost;dbname=$dbName",
+//    $dbUser,
+//    $dbPass
+//);
+//// hier vang die de error op
+//$db->setAttribute(
+//    PDO::ATTR_ERRMODE,
+//    PDO::ERRMODE_EXCEPTION
 
-/* data uit database terug naar vorig form */
+/* data uit database terug naar vorig form
+Haal alle data op uit de user tabellen welke beschikbaar zijn
 $marker = array(
-['lat'=>1.7788481178788205,'lng'=>51.60656920854833],
-['lat'=>3.7788481178788205,'lng'=>51.60656920854833],
-['lat'=>2.7788481178788205,'lng'=>51.60656920854833]
+    ['lat'=>1.7788481178788205,'lng'=>51.60656920854833],
+    ['lat'=>3.7788481178788205,'lng'=>51.60656920854833],
+    ['lat'=>2.7788481178788205,'lng'=>51.60656920854833]
 );
+*/
+
+$coords = \DB::select('SELECT lat, lon, name FROM users');
 
 
 
-/*data vanaf het formulier, dus update user table data */
-//$lat = $_GET['lat'];
-//$lon = $_GET['lng'];
 
-echo json_encode($marker);
+
+/*data vanaf het formulier, dus update user table data
+Plaats deze data in de user tabel doormiddel vam een update */
+$lat = $_GET['lat'];
+$lon = $_GET['lng'];
+
+echo json_encode($coords);
